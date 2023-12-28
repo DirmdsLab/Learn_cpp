265bd6d5e7dfed02ed81961e78ca0a590c587f9d
DirmdsLab <me.dirmdslab1204@gmail.com>
2023-12-28 13:46:55 +0700

    ^_^

---
+++ 01_Variabel.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int y = 5;
+    // dua di atas adalah variabel 
+
+    cout << "Nilai x = " << x << endl;
+    cout << "Nilai y = " << y << endl;
+    // dah itu aja variabel
+
+
+    return 0;
+}
