a25d802f8e7cd6903cba5436f85070326b3cd9db
DirmdsLab <me.dirmdslab1204@gmail.com>
2023-12-30 19:29:38 +0700

    Permulaan Hari ini

---
+++ 07_If.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+
+    if (x == 5) {
+        cout << "Nilai x = 5" << endl;
+    }
+
+    x = 10;
+
+    if (x == 5){
+        cout << "Nilai x = 5" << endl;
+    }
+
+    // Segitu aja dan selamat memahami :v 
+
+    return 0;
+}
