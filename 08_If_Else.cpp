e6d31e1b92b945488df79c8c8d364a6a7e485685
DirmdsLab <me.dirmdslab1204@gmail.com>
2023-12-31 21:26:47 +0700

    .

---
+++ 08_If_Else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 7;
+
+    if (x == 5){
+        cout << "x = 5" << endl;
+    } else {
+        cout << "x Bukan 5" << endl;
+    }
+    
+    // dah itu aja 
+    
+    return 0;
+}
