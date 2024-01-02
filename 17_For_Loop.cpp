257792d4e1aaa0350df2d437982c66de667b92df
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-02 22:49:26 +0700

    Loop

---
+++ 17_For_Loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+    // pengulangan, iya itu aja :v
+    for (int i = 0; i < 10; i++){  // bakal ngulang 10 kali
+        cout << "Print Angka : " << i << endl;
+    }
+    
+    cout << endl;
+
+    // part 2 
+    for (int i = 1; i <= 10; i++){  // bakal ngulang 10 kali
+        cout << "Print Angka : " << i << endl;
+    }
+
+    return 0;
+}
