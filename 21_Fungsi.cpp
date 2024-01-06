4f9e35585e58782b7b981749be9a6c5ae390a5c0
DirmdsLab <me.dirmdslab1204@gmail.com>
2024-01-06 21:10:45 +0700

    Yeee

---
+++ 21_Fungsi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath> // Awal Dari Pakai Lib Lain
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+    int Input;
+
+    cout << "Program Akar\n";
+    cout << "Masukan Angka : ";
+    cin >> Input;
+
+    cout << "Hasil Nya Adalah : " << sqrt(Input) << endl;
+
+    return 0;
+}
